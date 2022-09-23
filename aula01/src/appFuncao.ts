50de7c4f84392098377589e2162f73d00d08eaa9
Arian Weslley <weslleysantoszbd@gmail.com>
2022-09-23 09:06:24 -0300

    Função Anônima

--- aula01/src/appFuncao.ts
+++ aula01/src/appFuncao.ts
@@ -2,5 +2,12 @@
 function somar (x: number, y:number): number {
   return x + y;
 };
+console.log("Tradicional: O valor da soma é " + somar(6, 4));
 
-console.log("O valor da soma é " + somar(6, 4));
+//Função Anônima
+let fucao = function(x: number, y:number): number {
+  return x + y;
+};
+console.log("Anônima: O valor da soma é " + fucao(4,4));
+
+//Arrow function
