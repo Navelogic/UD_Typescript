3cd64cd48267f62dc83bb71b98c1ec5f4759784d
Arian Weslley <weslleysantoszbd@gmail.com>
2022-09-23 09:27:14 -0300

    Tratamento com =>

--- aula01/src/appMatriz.ts
+++ aula01/src/appMatriz.ts
@@ -1,4 +1,13 @@
 let nomes: Array<string> = ['Henrique', 'Beatriz', 'Arian'];
 console.log(nomes[1]);
 
+let numeros =[1, 5, 6, 9];
+
 nomes.forEach((x) => {console.log(x)});
+
+numeros.forEach((valor) => {
+  if(valor == 9){
+    console.log("Este valor é proibido");
+  };
+  console.log("Valor lido " + valor);
+});
