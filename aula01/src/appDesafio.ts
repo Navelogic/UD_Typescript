194fe1b90d37c68f729dd322a051d4c02c42ba37
Arian Weslley <weslleysantoszbd@gmail.com>
2022-09-24 19:23:41 -0300

    Revisão de Desafio

--- aula01/src/appDesafio.ts
+++ aula01/src/appDesafio.ts
@@ -1,7 +1,3 @@
-var n1 = document.getElementById('num1').nodeValue;
-var n2 = document.getElementById('num2').nodeValue;
-var n3 = document.getElementById('num3').nodeValue;
-
-let multiplicar = function (n1: number, n2: number, n3: number): number {
-  return (n1 * n2 * n3);
+function multiplicar(x: number, y: number, z: number): number {
+  return (x * y * z);
 }
