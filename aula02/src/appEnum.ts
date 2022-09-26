6dcedf32cd58d320945a65cacb64fc64f4ee0405
Arian Weslley <weslleysantoszbd@gmail.com>
2022-09-26 08:51:56 -0300

    Exemplo de Enum

---
+++ aula02/src/appEnum.ts
@@ -0,0 +1,12 @@
+enum Cor {
+  Verde = 10 ,
+  Amarelo = 12,
+  Vermelho = 14
+};
+let qualCor: string;
+
+qualCor = Cor[10];
+
+console.log("A cor escolhida foi " + qualCor);
+
+console.log(Cor.Verde);
