81e89bbbacb91f4f1fec269107850f0408beb63b
Arian Weslley <weslleysantoszbd@gmail.com>
2022-09-26 10:44:25 -0300

    appSemana e arquivo de notas

--- aula02/src/appSemana.ts
+++ aula02/src/appSemana.ts
@@ -1,5 +1,6 @@
-let semana = (dia: string = 'Domingo') => {
-  return dia;
-};
+let diasDaSemana: Array<string> = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado', 'domingo'];
 
+let semana = (dia: string = 'domingo') => {return dia;};
+
+console.log(`Hoje é ${semana(diasDaSemana[Math.floor(Math.random() * 6)])}`);
 console.log(`Hoje é ${semana()}`);
