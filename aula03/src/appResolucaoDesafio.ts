44d5b1c26ecb2128a9c271aae5a611e3454e4fca
Arian Weslley <weslleysantoszbd@gmail.com>
2022-09-28 08:40:24 -0300

    Resolução de valores

--- aula03/src/appResolucaoDesafio.ts
+++ aula03/src/appResolucaoDesafio.ts
@@ -1,11 +1,11 @@
 function resolucaoProblema(){
-  let numMax = 81;
+  let numMax = 6;
   let randomico = [];
 
-  for(let i = 0; i <= 5; i++){
+  for(let i = 0; i <= 4; i++){
     let temporario = Math.floor(Math.random() * numMax);
 
-    if(randomico.indexOf(temporario) == -1 && temporario > 0)
+    if((randomico.indexOf(temporario) == -1) && (temporario > 0))
     {
         randomico.push(temporario);
     } 
