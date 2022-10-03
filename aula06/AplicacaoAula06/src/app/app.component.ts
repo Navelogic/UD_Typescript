ab793f7f736b367a9ef3401a4885f8572b0036a1
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-03 11:26:31 -0300

    Mudança para Array

--- aula06/AplicacaoAula06/src/app/app.component.ts
+++ aula06/AplicacaoAula06/src/app/app.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'AA6-root',
   templateUrl: './app.component.html'
-  
+
 })
 export class AppComponent {
+  alunos = {nome: 'João do pé de feijão', aprovado: true};
   title = 'AplicacaoAula06';
-  nome = 'João do pé de feijão';
-  aprovado = true;
 }
