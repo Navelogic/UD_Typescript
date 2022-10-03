035b43584f20aa854570014f731a6e2e6fba77d3
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-03 10:29:04 -0300

    Criação de novo componente

--- aula06/AplicacaoAula06/src/app/app.module.ts
+++ aula06/AplicacaoAula06/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabecalhoComponentComponent } from './cabecalho-component/cabecalho-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CabecalhoComponentComponent,
   ],
   imports: [
     BrowserModule
---
+++ aula06/AplicacaoAula06/src/app/cabecalho-component/cabecalho-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'AA6-cabecalho-component',
+  templateUrl: './cabecalho-component.component.html',
+  styleUrls: ['./cabecalho-component.component.css']
+})
+export class CabecalhoComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
