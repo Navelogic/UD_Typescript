0aa32ab3d4692814b42642c562e55ec604f04c73
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-03 11:22:20 -0300

    CSS condicional

--- aula06/AplicacaoAula06/src/app/app.component.ts
+++ aula06/AplicacaoAula06/src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'AA6-root',
   templateUrl: './app.component.html'
+  
 })
 export class AppComponent {
   title = 'AplicacaoAula06';
+  nome = 'João do pé de feijão';
+  aprovado = true;
 }
