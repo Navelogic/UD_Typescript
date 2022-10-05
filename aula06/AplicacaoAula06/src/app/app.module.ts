2a7e174cc5b091eb459c1d4f237f5026e8396b0c
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-05 09:56:57 -0300

    Criação de componente de Estudantes

--- aula06/AplicacaoAula06/src/app/app.module.ts
+++ aula06/AplicacaoAula06/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CabecalhoComponentComponent } from './cabecalho-component/cabecalho-component.component';
+import { EstudantesComponent } from './estudantes/estudantes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponentComponent,
+    EstudantesComponent,
   ],
   imports: [
     BrowserModule
---
+++ aula06/AplicacaoAula06/src/app/estudantes/estudantes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'AA6-estudantes',
+  templateUrl: './estudantes.component.html',
+  styleUrls: ['./estudantes.component.css']
+})
+export class EstudantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
