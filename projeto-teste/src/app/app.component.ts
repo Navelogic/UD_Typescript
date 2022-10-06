f1b45969b05201bbda27eab3a77560bb30ad85fb
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-05 23:17:39 -0300

    publico e private

--- projeto-teste/src/app/app.component.ts
+++ projeto-teste/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projeto-teste';
+
+  public title="Bem-Vindo!";
+  private teste="Esse é privado!";
+
 }
