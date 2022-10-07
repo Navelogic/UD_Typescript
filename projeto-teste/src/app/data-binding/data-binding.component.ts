32ae253cd9e58fb8007e177b27a1b96322acef61
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-07 20:44:13 -0300

    Aplicação de Imagem com Property Binding

--- projeto-teste/src/app/data-binding/data-binding.component.ts
+++ projeto-teste/src/app/data-binding/data-binding.component.ts
@@ -12,6 +12,8 @@ export class DataBindingComponent implements OnInit {
   public maisUm: number = 20;
 
   public checagem: boolean = false;
+  public imgSrc: string = "https://imgs.search.brave.com/OpbZmlXIAvtaE9bqUSrcx9LTCFUT5NJ3tLqfNLYOBI8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly8xLmJw/LmJsb2dzcG90LmNv/bS8tMEtxT3JpVkxL/SE0vVzE2TjdHY1gz/a0kvQUFBQUFBQUFy/Slkvd010ZkdiNXV1/UmtiaXVKOUw4UmRL/cjh2VTctZjREY01n/Q0V3WUJoZ0wvczE2/MDAvMjFiJTJCemVi/cmElMkItJTJCSHdh/bmdlLmpwZw";
+
 
   constructor() { }
 
