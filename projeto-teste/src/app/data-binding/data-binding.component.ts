be8999017571ca65e22c61dbcf78b3636d18f31f
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-07 20:46:06 -0300

    Alterando propriedades do HTML com Property Binding

--- projeto-teste/src/app/data-binding/data-binding.component.ts
+++ projeto-teste/src/app/data-binding/data-binding.component.ts
@@ -14,7 +14,7 @@ export class DataBindingComponent implements OnInit {
   public checagem: boolean = false;
   public imgSrc: string = "https://imgs.search.brave.com/OpbZmlXIAvtaE9bqUSrcx9LTCFUT5NJ3tLqfNLYOBI8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly8xLmJw/LmJsb2dzcG90LmNv/bS8tMEtxT3JpVkxL/SE0vVzE2TjdHY1gz/a0kvQUFBQUFBQUFy/Slkvd010ZkdiNXV1/UmtiaXVKOUw4UmRL/cjh2VTctZjREY01n/Q0V3WUJoZ0wvczE2/MDAvMjFiJTJCemVi/cmElMkItJTJCSHdh/bmdlLmpwZw";
 
-
+  public imgTitle: string = "Property Binding";
   constructor() { }
 
   ngOnInit(): void {
