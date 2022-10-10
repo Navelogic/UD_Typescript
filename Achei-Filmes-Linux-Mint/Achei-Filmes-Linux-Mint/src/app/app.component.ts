26139abe53afc8ace29f218477cb23326c1e8022
Navelogic <weslleysantoszbd@gmail.com>
2022-10-10 16:24:15 -0300

    Criação de Projeto Linux Mint

---
+++ Achei-Filmes-Linux-Mint/Achei-Filmes-Linux-Mint/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'acf-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Achei-Filmes-Linux-Mint';
+}
