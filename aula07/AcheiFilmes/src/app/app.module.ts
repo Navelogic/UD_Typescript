a40a85cb77a7ba3c05691a8eb0a13f29f5dde4a5
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-10 11:09:29 -0300

    Criação de componente Sobre

--- aula07/AcheiFilmes/src/app/app.module.ts
+++ aula07/AcheiFilmes/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HeroComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule
