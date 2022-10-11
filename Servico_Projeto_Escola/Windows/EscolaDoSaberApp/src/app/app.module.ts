3c1411993875019f60f6d31ba3962abb4b1c6a63
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-11 09:05:32 -0300

    Criação de C receber

--- Servico_Projeto_Escola/Windows/EscolaDoSaberApp/src/app/app.module.ts
+++ Servico_Projeto_Escola/Windows/EscolaDoSaberApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CadastroUserComponent } from './cadastro-user/cadastro-user.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
+import { ReceberComponent } from './receber/receber.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { ROUTES } from './app.routes';
     CadastroComponent,
     AlteracaoComponent,
     CadastroUserComponent,
-    SobreComponent
+    SobreComponent,
+    ReceberComponent
   ],
   imports: [
     BrowserModule,
---
+++ Servico_Projeto_Escola/Windows/EscolaDoSaberApp/src/app/receber/receber.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eds-receber',
+  templateUrl: './receber.component.html',
+  styleUrls: ['./receber.component.css']
+})
+export class ReceberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
