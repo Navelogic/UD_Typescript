ec3a61c9d72d79c673f59d0faf5554ab8b82c07b
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-11 08:53:22 -0300

    Criação C footer

--- Servico_Projeto_Escola/Windows/EscolaDoSaberApp/src/app/app.module.ts
+++ Servico_Projeto_Escola/Windows/EscolaDoSaberApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
