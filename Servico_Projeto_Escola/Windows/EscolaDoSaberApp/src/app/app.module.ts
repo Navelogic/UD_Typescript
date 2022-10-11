5b7952f40739de7acca50f3d6db89784493e95d2
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-11 08:53:59 -0300

    Criação C hero

--- Servico_Projeto_Escola/Windows/EscolaDoSaberApp/src/app/app.module.ts
+++ Servico_Projeto_Escola/Windows/EscolaDoSaberApp/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FooterComponent
+    FooterComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
