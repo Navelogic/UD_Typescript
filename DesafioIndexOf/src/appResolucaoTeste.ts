54b7b6dd993fded74ec0f4893582fd7ff776c874
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-12 23:57:13 -0300

    Remoção do Desafio

--- DesafioIndexOf/src/appResolucaoTeste.ts
+++
@@ -1,9 +0,0 @@
-import { Buscar } from "./appResolucao";
-
-let numeros: Array<number> = [3, 5, 7, 9, 11, 13, 15];
-let x: number = 2;
-
-let b: Buscar = new Buscar();
-let achei: boolean = b.existeValor(numeros, x);
-
-console.log(`Encontrou o valor? ${(achei == false) ? 'Não' : 'Sim'}`);
