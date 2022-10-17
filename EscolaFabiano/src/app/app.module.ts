adb5fb1a6e35f2ca68c951966c6582529b8ee2d2
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-17 11:02:19 -0300

    Criação de componentes

--- EscolaFabiano/src/app/app.module.ts
+++ EscolaFabiano/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    SobreComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule
