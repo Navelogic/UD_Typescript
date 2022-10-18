64b001fc1aa3477eba221473d5ecc6e6fd52c095
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-18 09:05:36 -0300

    Finalização de Tratamento de Erro

--- EscolaFabiano/src/app/alunos/alunos.service.ts
+++ EscolaFabiano/src/app/alunos/alunos.service.ts
@@ -2,7 +2,8 @@ import { Aluno } from "./aluno/aluno.model";
 import { ESCOLA_FABIANO_API } from "../app.api";
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-import { Observable } from "rxjs";
+import { catchError, Observable } from "rxjs";
+import { ErrorHandler } from "../app.error-handler";
 
 @Injectable()
 export class AlunosService {
@@ -11,7 +12,7 @@ export class AlunosService {
   constructor(private http: HttpClient) {}
 
   listarAlunos(): Observable<Aluno[]> {
-    return this.http.get<Aluno[]>(`${ESCOLA_FABIANO_API}/aluno/listar`)
+    return this.http.get<Aluno[]>(`${ESCOLA_FABIANO_API}/aluno/listar`).pipe(catchError(ErrorHandler.handleError));
   }
 
 }
